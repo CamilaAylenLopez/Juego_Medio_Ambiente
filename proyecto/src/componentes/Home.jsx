498133100e59bc2e7f8f214aff053384d5b55e72
CamilaAylenLopez <camilalopezort@gmail.com>
2023-08-27 00:32:07 -0300

    página de info arreglada

--- proyecto/src/componentes/Home.jsx
+++ proyecto/src/componentes/Home.jsx
@@ -10,7 +10,7 @@ const Home = (props) => {
             <div className='centrar'>
                 <div className='cuadro'>
                     <p className='texto'>El medioambiente es el espacio en el que se desarrolla la vida de los distintos organismos favoreciendo su interacción. En él se encuentran tanto seres vivos como elementos sin vida y otros creados por la mano del hombre.</p>
-                    <p className='texto'>Por eso creamos esta página, para concentizar a través de información y juegos.</p>
+                    <p className='texto'>Por eso creamos esta página, para concentizar a través de información y juegos para niños.</p>
                 </div>
             </div>
         </>
