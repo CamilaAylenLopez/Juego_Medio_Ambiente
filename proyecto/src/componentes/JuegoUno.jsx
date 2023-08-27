6d25d3c3450d8515d4cd92ea6f41b1d439953dc6
CamilaAylenLopez <camilalopezort@gmail.com>
2023-08-27 16:17:20 -0300

    juego uno terminado ahora si creo

--- proyecto/src/componentes/JuegoUno.jsx
+++ proyecto/src/componentes/JuegoUno.jsx
@@ -17,7 +17,41 @@ const JuegoUno = (props) => {
     tacho: 'marron',
     imagen: 'https://cloudfront-us-east-1.images.arcpublishing.com/semana/V4Z6O774F5BKLDY2ZNKEHE6MVI.jpg'
   },
-
+  {
+    objeto: 'botella de vidrio',
+    tacho: 'verde',
+    imagen: 'https://www.laeditorial.com.ar/21963-home_default/botella-vidrio-500cc-jugo-agua-leche-t-38.jpg'
+  },
+  {
+    objeto: 'pañal usado',
+    tacho: 'negro',
+    imagen: 'https://www.webconsultas.com/sites/default/files/styles/wc_adaptive_image__small/public/temas/panales-que-son_0.jpg'
+  },
+  {
+    objeto: 'diario',
+    tacho: 'azul',
+    imagen: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Primera_edici%C3%B3n_del_diario_argentino_Clar%C3%ADn.jpg'
+  },
+  {
+    objeto: 'tuerca',
+    tacho: 'amarillo',
+    imagen: 'https://www.bmbulmetal.com.ar/database/articulos/fotos/62/TUERCA-HEXAGONAL-GRADO2-HIERRO-UNC-UNF-WTW-CALIDAD8.8-METRICO-PULGADA-MA-MB-MC.jpg'
+  },
+  {
+    objeto: 'lata',
+    tacho: 'amarillo',
+    imagen: 'https://img.freepik.com/vector-premium/lata-aluminio-brillante-refresco-o-plantilla-cerveza-blanco_88653-860.jpg'
+  },
+  {
+    objeto: 'pañuelo',
+    tacho: 'negro',
+    imagen: 'https://s03.s3c.es/imag/_v0/770x420/8/8/9/GettyImages-97611377.jpg'
+  },
+  {
+    objeto: 'botella de plastico',
+    tacho: 'verde',
+    imagen: 'https://d100mj7v0l85u5.cloudfront.net/s3fs-public/sidelg.jpg'
+  },
   ])
 
   useEffect(() => {
