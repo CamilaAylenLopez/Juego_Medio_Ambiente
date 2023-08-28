a7d34b0abd292ffb40e1d8332dacbfa9763a8afd
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-28 10:49:55 -0300

    mapa

--- proyecto/src/componentes/PuntosVerdes.jsx
+++ proyecto/src/componentes/PuntosVerdes.jsx
@@ -23,6 +23,11 @@ const PuntosVerdes = () => {
     }
 
     navigator.geolocation.getCurrentPosition(success, error, options);
+    return(
+        <>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d13133.343686924292!2d-58.4287612!3d-34.6209504!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1693230312933!5m2!1ses!2sar" width="600" height="450" allowFullScreen=""  loading="lazy" className='centrarJust'></iframe>
+        </>
+    )
 }
 
 
